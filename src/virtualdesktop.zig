6ba4b85bcc152ce89ed124ff9994d2d8695678fb
Timur Sultanov <artifth@artifth.ru>
2020-04-17 22:16:45 +0300

    Extracting common COM constants

--- src/virtualdesktop.zig
+++ src/virtualdesktop.zig
@@ -1,33 +1,6 @@
 const std = @import("std");
 const w = @import("win32").c;
-
-const ComError = error {
-    FailedToCreateComObject
-};
-
-const CLSCTX_INPROC_SERVER      = 0x1;
-const CLSCTX_INPROC_HANDLER     = 0x2;
-const CLSCTX_LOCAL_SERVER       = 0x4;
-const CLSCTX_INPROC_SERVER16    = 0x8;
-const CLSCTX_REMOTE_SERVER      = 0x10;
-const CLSCTX_INPROC_HANDLER16       = 0x20;
-const CLSCTX_RESERVED1          = 0x40;
-const CLSCTX_RESERVED2          = 0x80;
-const CLSCTX_RESERVED3          = 0x100;
-const CLSCTX_RESERVED4          = 0x200;
-const CLSCTX_NO_CODE_DOWNLOAD       = 0x400;
-const CLSCTX_RESERVED5          = 0x800;
-const CLSCTX_NO_CUSTOM_MARSHAL      = 0x1000;
-const CLSCTX_ENABLE_CODE_DOWNLOAD   = 0x2000;
-const CLSCTX_NO_FAILURE_LOG     = 0x4000;
-const CLSCTX_DISABLE_AAA        = 0x8000;
-const CLSCTX_ENABLE_AAA         = 0x10000;
-const CLSCTX_FROM_DEFAULT_CONTEXT   = 0x20000;
-const CLSCTX_ACTIVATE_32_BIT_SERVER = 0x40000;
-const CLSCTX_ACTIVATE_64_BIT_SERVER = 0x80000;
-const CLSCTX_INPROC         = CLSCTX_INPROC_SERVER|CLSCTX_INPROC_HANDLER;
-const CLSCTX_SERVER         = CLSCTX_INPROC_SERVER|CLSCTX_LOCAL_SERVER|CLSCTX_REMOTE_SERVER;
-const CLSCTX_ALL            = CLSCTX_SERVER|CLSCTX_INPROC_HANDLER;
+const com = @import("com.zig");
 
 const CLSID_VirtualDesktopManager: w.CLSID = w.CLSID {
     .Data1 = 0xaa509086,
@@ -64,10 +37,8 @@ const IID_IVirtualDesktopManagerInternal = w.IID {
     .Data4 = [8]u8 {0xB7, 0x12, 0xF8, 0x4B, 0x63, 0x5B, 0xEB, 0xE7},
 };
 
-const REFIID = [*c]const w.IID;
-
 const IVirtualDesktopManagerVtbl = extern struct {
-    QueryInterface: extern fn (This: [*c]IVirtualDesktopManager, riid: REFIID, ppvObject: [*c]?*c_void) callconv(.C) w.HRESULT,
+    QueryInterface: extern fn (This: [*c]IVirtualDesktopManager, riid: com.REFIID, ppvObject: [*c]?*c_void) callconv(.C) w.HRESULT,
     AddRef: extern fn (This: [*c]IVirtualDesktopManager) callconv(.C) w.ULONG,
     Release: extern fn (This: [*c]IVirtualDesktopManager) callconv(.C) w.ULONG,
     IsWindowOnCurrentVirtualDesktop: extern fn (This: [*c]IVirtualDesktopManager, topLevelWindow: w.HWND, onCurrentDesktop: [*c]w.BOOL) callconv(.C) w.HRESULT,
@@ -78,7 +49,7 @@ const IVirtualDesktopManagerVtbl = extern struct {
 const IVirtualDesktopManager = extern struct {
     lpVtbl: [*c]IVirtualDesktopManagerVtbl,
         
-    pub fn QueryInterface(self: *IVirtualDesktopManager, riid: REFIID, ppvObject: [*c][*c]c_void) w.HRESULT {
+    pub fn QueryInterface(self: *IVirtualDesktopManager, riid: com.REFIID, ppvObject: [*c][*c]c_void) w.HRESULT {
         return self.lpVtbl.*.QueryInterface(self, riid, ppvObject);
     }
     pub fn AddRef(self: *IVirtualDesktopManager) w.ULONG {
@@ -101,6 +72,9 @@ const IVirtualDesktopManager = extern struct {
 const IApplicationView = @OpaqueType();
 
 const IVirtualDesktopVtbl = extern struct {
+    QueryInterface: extern fn (This: [*c]IVirtualDesktop, riid: com.REFIID, ppvObject: [*c]?*c_void) callconv(.C) w.HRESULT,
+    AddRef: extern fn (This: [*c]IVirtualDesktop) callconv(.C) w.ULONG,
+    Release: extern fn (This: [*c]IVirtualDesktop) callconv(.C) w.ULONG,
     IsViewVisible: extern fn (This: [*c]IVirtualDesktop, pView: [*c]IApplicationView, pfVisible: [*c]c_int) callconv(.C) w.HRESULT,
     GetID: extern fn (This: [*c]IVirtualDesktop, pGuid: [*c]w.GUID) callconv(.C) w.HRESULT,
 };
@@ -108,6 +82,15 @@ const IVirtualDesktopVtbl = extern struct {
 const IVirtualDesktop = extern struct {
     lpVtbl: [*c]IVirtualDesktopVtbl,
 
+    pub fn QueryInterface(self: *IVirtualDesktop, riid: com.REFIID, ppvObject: [*c][*c]c_void) w.HRESULT {
+        return self.lpVtbl.*.QueryInterface(self, riid, ppvObject);
+    }
+    pub fn AddRef(self: *IVirtualDesktop) w.ULONG {
+        return self.lpVtbl.*.AddRef(self);
+    }
+    pub fn Release(self: *IVirtualDesktop) w.ULONG {
+        return self.lpVtbl.*.Release(self);
+    }
     pub fn IsViewVisible(self: *IVirtualDesktop, pView: [*c]IApplicationView, pfVisible: [*c]c_int) w.HRESULT {
         return self.lpVtbl.*.IsViewVisible(self, pView, pfVisible);
     }
@@ -122,6 +105,9 @@ const AdjacentDesktop = enum {
 };
 
 const IVirtualDesktopManagerInternalVtbl = extern struct {
+    QueryInterface: extern fn (This: [*c]IVirtualDesktopManagerInternal, riid: com.REFIID, ppvObject: [*c]?*c_void) callconv(.C) w.HRESULT,
+    AddRef: extern fn (This: [*c]IVirtualDesktopManagerInternal) callconv(.C) w.ULONG,
+    Release: extern fn (This: [*c]IVirtualDesktopManagerInternal) callconv(.C) w.ULONG,
     GetCount: extern fn(This: [*c]IVirtualDesktopManagerInternal, pCount: [*c]c_int) callconv(.C) w.HRESULT,
     MoveViewDesktop: extern fn(This: [*c]IVirtualDesktopManagerInternal, pView: [*c]IApplicationView, pDesktop: [*c]IVirtualDesktop) callconv(.C) w.HRESULT,
     GetCurrentDesktop: extern fn(This: [*c]IVirtualDesktopManagerInternal, desktop: [*c][*c]IVirtualDesktop) callconv(.C) w.HRESULT,
@@ -134,14 +120,48 @@ const IVirtualDesktopManagerInternalVtbl = extern struct {
 
 const IVirtualDesktopManagerInternal = extern struct {
     lpVtbl: [*c]IVirtualDesktopManagerInternalVtbl,
+
+    pub fn QueryInterface(self: *IVirtualDesktopManagerInternal, riid: com.REFIID, ppvObject: [*c][*c]c_void) w.HRESULT {
+        return self.lpVtbl.*.QueryInterface(self, riid, ppvObject);
+    }
+    pub fn AddRef(self: *IVirtualDesktopManagerInternal) w.ULONG {
+        return self.lpVtbl.*.AddRef(self);
+    }
+    pub fn Release(self: *IVirtualDesktopManagerInternal) w.ULONG {
+        return self.lpVtbl.*.Release(self);
+    }
+    pub fn GetCount(self: *IVirtualDesktopManagerInternal, pCount: [*c]c_int) w.HRESULT {
+        return self.lpVtbl.*.GetCount(self, pCount);
+    }
+    pub fn MoveViewDesktop(self: *IVirtualDesktopManagerInternal, pView: [*c]IApplicationView, pDesktop: [*c]IVirtualDesktop) w.HRESULT {
+        return self.lpVtbl.*.MoveViewDesktop(self, pView, pDesktop);
+    }
+    pub fn GetCurrentDesktop(self: *IVirtualDesktopManagerInternal, desktop: [*c][*c]IVirtualDesktop) w.HRESULT {
+        return self.lpVtbl.*.GetCurrentDesktop(self, desktop);
+    }
+    pub fn GetDesktops(self: *IVirtualDesktopManagerInternal, ppDesktops: [*c][*c]IVirtualDesktop) w.HRESULT {
+        return self.lpVtbl.*.GetDesktops(self, ppDesktops);
+    }
+    pub fn GetAdjacentDesktop(self: *IVirtualDesktopManagerInternal, pDesktopReference: [*c]IVirtualDesktop, uDirection: AdjacentDesktop, ppAdjacentDesktop: [*c][*c]IVirtualDesktop) w.HRESULT {
+        return self.lpVtbl.*.GetAdjacentDesktop(self, pDesktopReference, uDirection, ppAdjacentDesktop);
+    }
+    pub fn SwitchDesktop(self: *IVirtualDesktopManagerInternal, pDesktop: [*c]IVirtualDesktop) w.HRESULT {
+        return self.lpVtbl.*.SwitchDesktop(self, pDesktop);
+    }
+    pub fn CreateDesktopW(self: *IVirtualDesktopManagerInternal, ppNewDesktop: [*c][*c]IVirtualDesktop) w.HRESULT {
+        return self.lpVtbl.*.CreateDesktopW(self, ppNewDesktop);
+    }
+    pub fn RemoveDesktop(self: *IVirtualDesktopManagerInternal, pRemove: [*c]IVirtualDesktop, pFallbackDesktop: [*c]IVirtualDesktop) w.HRESULT {
+        return self.lpVtbl.*.RemoveDesktop(self, pRemove, pFallbackDesktop);
+    }
 };
 
 pub fn create() !*IVirtualDesktopManager {
     var virtualDesktopManager: *IVirtualDesktopManager = undefined;
-    var hr = w.CoCreateInstance(&CLSID_VirtualDesktopManager, null, CLSCTX_ALL, &IID_IVirtualDesktopManager, @intToPtr([*c]?*c_void, @ptrToInt(&virtualDesktopManager)));
+    var hr = w.CoCreateInstance(&CLSID_VirtualDesktopManager, null, com.CLSCTX_ALL, &IID_IVirtualDesktopManager, @intToPtr([*c]?*c_void, @ptrToInt(&virtualDesktopManager)));
     if (hr == 0) {
         return virtualDesktopManager;
     } else {
-        return ComError.FailedToCreateComObject;
+        return com.ComError.FailedToCreateComObject;
     }
 }
