1f2290f3c0650972158d55281ee9f7553a755587
Timur Sultanov <artifth@artifth.ru>
2020-06-27 00:12:22 +0300

    Draft of event handlers

--- src/window.zig
+++ src/window.zig
@@ -8,11 +8,12 @@ fn WindowProc(hwnd: w.HWND, uMsg: w.UINT, wParam: w.WPARAM, lParam: w.LPARAM) ca
         return w.DefWindowProcW(hwnd, uMsg, wParam, lParam);
     }
 
-    var wndProc = @intToPtr(WndProc, @bitCast(usize, wLong));
-    return wndProc(hwnd, uMsg, wParam, lParam);
+    var window = @intToPtr(*Window, @bitCast(usize, wLong));
+
+    return window.wndProc(uMsg, wParam, lParam) catch return 1;
 }
 
-pub const WndProc = fn (hWnd: w.HWND, uMsg: w.UINT, wParam: w.WPARAM, lParam: w.LPARAM) w.LRESULT;
+pub const WndProc = fn (self: Window, uMsg: w.UINT, wParam: w.WPARAM, lParam: w.LPARAM) w.LRESULT;
 
 pub const WindowParameters = struct {
     exStyle: w.DWORD = 0,
@@ -21,18 +22,59 @@ pub const WindowParameters = struct {
     style: w.DWORD = w.WS_OVERLAPPEDWINDOW,
     x: c_int = 100,
     y: c_int = 100,
-    width: c_int = 100,
-    height: c_int = 100,
+    width: c_int = 640,
+    height: c_int = 480,
     parent: ?Window = null,
     menu: w.HMENU = null,
-    wndProc: WndProc
+};
+
+fn defaultHandler(window: Window) !void {}
+
+fn onResizeHandler(window: Window) !void {
+    for(window.children.items) |wnd| {
+        window.dockChild(wnd);
+    }
+}
+
+pub const WindowEventHandlers = struct {
+    onClick: fn (window: Window) anyerror!void = defaultHandler,
+    onResize: fn (window: Window) anyerror!void = onResizeHandler,
+    onCreate: fn(window: Window) anyerror!void = defaultHandler,
+    onDestroy: fn (window: Window) anyerror!void = defaultHandler
 };
 
 pub const Window = struct {
     hwnd: w.HWND,
     hInstance: w.HINSTANCE,
+    dock: bool = false,
+    eventHandlers: WindowEventHandlers,
+    children: *std.ArrayList(*Window),
 
-    pub fn create(windowParameters: WindowParameters, hInstance: w.HINSTANCE) Window {
+    pub fn wndProc(self: Window, uMsg: w.UINT, wParam: w.WPARAM, lParam: w.LPARAM) !w.LRESULT {
+        switch(uMsg) {
+            w.WM_SIZE => {
+                try self.eventHandlers.onResize(self);
+                return 0;
+            },
+            w.WM_LBUTTONDOWN => {
+                try self.eventHandlers.onClick(self);
+                return 0;
+            },
+            w.WM_CREATE => {
+                try self.eventHandlers.onCreate(self);
+                return 0;
+            },
+            w.WM_DESTROY => {
+                try self.eventHandlers.onDestroy(self);
+                return 0;
+            },
+            else => {
+                return w.DefWindowProcW(self.hwnd, uMsg, wParam, lParam);
+            }
+        }
+    }
+
+    pub fn create(windowParameters: WindowParameters, eventHandlers: WindowEventHandlers, hInstance: w.HINSTANCE, allocator: *std.mem.Allocator) !*Window {
         const wc: w.WNDCLASSW = .{
             .style = 0,
             .lpfnWndProc = WindowProc,
@@ -50,12 +92,19 @@ pub const Window = struct {
 
         var hwnd = w.CreateWindowExW(windowParameters.exStyle, windowParameters.className, windowParameters.title, windowParameters.style, windowParameters.x, windowParameters.y, windowParameters.width, windowParameters.height, if(windowParameters.parent) |p| p.hwnd else null, windowParameters.menu, hInstance, null);
 
-        _ = w.SetWindowLongPtr(hwnd, w.GWLP_USERDATA, @bitCast(c_longlong, @ptrToInt(windowParameters.wndProc)));
-
-        return Window{
+        var children = try allocator.create(std.ArrayList(*Window));
+        children.* = std.ArrayList(*Window).init(allocator);
+        var window = try allocator.create(Window);
+        window.* = Window {
             .hwnd = hwnd,
             .hInstance = hInstance,
+            .children = children,
+            .eventHandlers = eventHandlers
         };
+
+        _ = w.SetWindowLongPtr(hwnd, w.GWLP_USERDATA, @bitCast(c_longlong, @ptrToInt(window)));
+
+        return window;
     }
 
     pub fn show(self: Window) void {
@@ -77,9 +126,6 @@ pub const Window = struct {
     }
 
     pub fn redraw(self: Window) void {
-        //var rect = self.getRect();
-        //var rgn = self.getRgn();
-        //std.debug.warn("Redraw on {} {} {} {}\n", .{rect.left, rect.right, rect.top, rect.bottom});
         _ = w.RedrawWindow(self.hwnd, null, null, w.RDW_INVALIDATE | w.RDW_UPDATENOW);
     }
 
@@ -114,14 +160,5 @@ pub const Window = struct {
 
     pub fn destroy(self: Window) void {
         _ = w.DestroyWindow(self.hwnd);
-    }
-
-    pub fn loop(self: Window) void {
-        var msg: w.MSG = undefined;
-
-        while (w.GetMessageW(&msg, self.hwnd, 0, 0) != 0) {
-            _ = w.TranslateMessage(&msg);
-            _ = w.DispatchMessageW(&msg);
-        }
     }
 };
--- src/wmain.zig
+++ src/wmain.zig
@@ -2,25 +2,21 @@ const std = @import("std");
 const w = @import("win32").c;
 const Window = @import("window.zig").Window;
 const WindowParameters = @import("window.zig").WindowParameters;
+const WindowEventHandlers = @import("window.zig").WindowEventHandlers;
 
-fn WindowProc(hwnd: w.HWND, uMsg: w.UINT, wParam: w.WPARAM, lParam: w.LPARAM) w.LRESULT {
-    return switch (uMsg) {
-        w.WM_DESTROY => {
-            return 0;
-        },
-        w.WM_CREATE => {
-            return 0;
-        },
-        else => w.DefWindowProcW(hwnd, uMsg, wParam, lParam)
-    };
+fn onDestroyHandler(window: Window) !void {
+    _ = w.PostQuitMessage(0);
 }
 
 pub export fn WinMain(hInstance: w.HINSTANCE, hPrevInstance: w.HINSTANCE, pCmdLine: w.LPWSTR, nCmdShow: c_int) callconv(.C) c_int {
-    const stdin = std.io.getStdIn().inStream();
-    _ = stdin.readByte() catch unreachable;
+    //const stdin = std.io.getStdIn().inStream();
+    //_ = stdin.readByte() catch unreachable;
     var hr = w.CoInitializeEx(null, 0x2);
 
-    var window = Window.create(WindowParameters {.wndProc = WindowProc}, hInstance);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    var window = Window.create(WindowParameters {}, WindowEventHandlers { .onDestroy = onDestroyHandler }, hInstance, &arena.allocator) catch unreachable;
     window.show();
 
     var msg: w.MSG = undefined;
